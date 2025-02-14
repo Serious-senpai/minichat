45a48374e7c3dc90211327e630c79494fea29725
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-02-14 21:43:01 +0700

    Fix deployment

--- services/api/scripts/install.sh
+++ services/api/scripts/install.sh
@@ -6,5 +6,11 @@ API_SERVICE_DIR=$(realpath $SCRIPT_DIR/..)
 
 echo "Got root of API service: $API_SERVICE_DIR"
 
-sudo python -m venv /venv
-sudo /venv/bin/pip install -r $API_SERVICE_DIR/requirements.txt
+if [ "$EUID" -eq 0 ]
+then
+    python -m venv /venv
+else
+    sudo python -m venv /venv
+fi
+
+/venv/bin/pip install -r $API_SERVICE_DIR/requirements.txt
