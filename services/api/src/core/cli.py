fe53fd64454af4cdabd99aa764e341c5a7ee0332
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-02-25 14:35:06 +0000

    Do not spawn API workers by default

--- services/api/src/core/cli.py
+++ services/api/src/core/cli.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import argparse
-from typing import TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING
 
 
 __all__ = ("namespace", "parse_args")
@@ -11,7 +11,7 @@ class __Namespace(argparse.Namespace):
     if TYPE_CHECKING:
         host: str
         port: int
-        workers: int
+        workers: Optional[int]
         log_level: str
         data_service: str
         amqp_host: str
@@ -24,7 +24,7 @@ __parser = argparse.ArgumentParser(
 )
 __parser.add_argument("--host", type=str, default="0.0.0.0", help="The host to bind the HTTP server to")
 __parser.add_argument("--port", type=int, default=8000, help="The port to bind the HTTP server to")
-__parser.add_argument("--workers", type=int, default=2, help="The number of worker processes to run")
+__parser.add_argument("--workers", type=int, required=False, help="The number of worker processes to run")
 __parser.add_argument("--log-level", type=str, default="debug", help="The log level for the application")
 __parser.add_argument("--data-service", type=str, default="localhost:16000", help="The URL to the data service to pass to `grpc.aio.insecure_channel`")
 __parser.add_argument("--amqp-host", type=str, default="amqp://guest:guest@rabbitmq-proxy:5672", help="The URL to RabbitMQ to pass to `aio_pika.connect_robust`")
